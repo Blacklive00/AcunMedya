9f77aa68b41e7b1a727e0dbb1989c9a084990031
Blacklive00 <102056225+Blacklive00@users.noreply.github.com>
2025-05-02 00:02:59 +0300

    yeni proje
    abstract

---
+++ 13ACUN MEDYA AKADEMI/Attribute  Kişisel Bilgi Doğrulama/Attribute  Kişisel Bilgi Doğrulama/GerekliAlanAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public sealed class GerekliAlanAttribute : Attribute
+{
+    public string HataMesaji { get; }
+
+    public GerekliAlanAttribute(string hataMesaji)
+    {
+        HataMesaji = hataMesaji;
+    }
+}
